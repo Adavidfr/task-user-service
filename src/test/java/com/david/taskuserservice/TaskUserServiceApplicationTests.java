88ec29107d47d76086eaa4b6ae5cefc6470497fb
Alex David Frías <adavidfruiz@gmail.com>
2024-05-15 23:39:51 -0500

    feat: add project

---
+++ src/test/java/com/david/taskuserservice/TaskUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.david.taskuserservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskUserServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
