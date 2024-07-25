05b38ec50334ec6808e40cf9e9c548ed9110e676
Alex David Frías <adavidfruiz@gmail.com>
2024-07-25 02:08:56 -0500

    feat: add user service implementation

---
+++ src/main/java/com/david/taskuserservice/service/UserServiceImpl.java
@@ -0,0 +1,27 @@
+package com.david.taskuserservice.service;
+
+import com.david.taskuserservice.config.JwtProvider;
+import com.david.taskuserservice.model.User;
+import com.david.taskuserservice.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserServiceImpl implements UserService{
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public User getUserProfile(String jwt) {
+        String email = JwtProvider.getEmailFromJwtToken(jwt);
+        return userRepository.findByEmail(email);
+    }
+
+    @Override
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+}
