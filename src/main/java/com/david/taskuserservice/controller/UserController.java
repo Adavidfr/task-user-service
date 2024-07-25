c035165e70fbc9a437a19b1eb968863dcd3ed413
Alex David Frías <adavidfruiz@gmail.com>
2024-07-25 02:08:23 -0500

    feat: add user controller

--- src/main/java/com/david/taskuserservice/controller/UserController.java
+++ src/main/java/com/david/taskuserservice/controller/UserController.java
@@ -1,4 +1,32 @@
 package com.david.taskuserservice.controller;
 
+import com.david.taskuserservice.model.User;
+import com.david.taskuserservice.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/user")
 public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/profile")
+    public ResponseEntity<User> getUserProfile(@RequestHeader("Authorization") String jwt) {
+
+        User user = userService.getUserProfile(jwt);
+        return new ResponseEntity<>(user, HttpStatus.OK);
+    }
+
+    @GetMapping()
+    public ResponseEntity<List<User>> getUsers(@RequestHeader("Authorization") String jwt) {
+
+        List<User> users = userService.getAllUsers();
+        return new ResponseEntity<>(users, HttpStatus.OK);
+    }
 }
